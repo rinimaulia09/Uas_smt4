b7f2dab4782343c7ca7e7ee44c9661c30a4d5f6c
rinimaulia09 <rinimaulia09@gmail.com>
2024-05-04 21:17:45 +0700

    Menambahkan halaman dan bottom tab navigation

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Home from './screens/Home'
+import Profil from './screens/Profil'
+import { NavigationContainer } from '@react-navigation/native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Notifikasi from './screens/Notifikasi'
+import Pesanan from './screens/Pesanan'
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+   <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name='Home' component={Home} options={{
+          tabBarLabel: 'Beranda',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+      
+      }} />
+      
+      <Tab.Screen name='Profil' component={Profil} options={{
+          tabBarLabel: 'Profil',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+      
+      }} />
+      
+    </Tab.Navigator>
+   </NavigationContainer>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
